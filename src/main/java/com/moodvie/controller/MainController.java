58a323f7528f24bcc9970d99773d83fe8be6ff2f
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-12-28 14:37:06 +0100

    ✨ feat: Add all types of subscribe on initialisation of the app

--- src/main/java/com/moodvie/controller/MainController.java
+++ src/main/java/com/moodvie/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.moodvie.controller;
 
 
+import com.moodvie.business.facade.TypeSubscribeFacade;
+
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
 
@@ -9,8 +11,20 @@ public class MainController {
     private StackPane contentArea;
 
     public void initialize() {
+        // Create TypeOfSubscribe instances
+        createTypeOfSubscribeInstances();
+        
         NavigationController.getInstance().setMainContentArea(contentArea);
         // Chargez la premiere vue
         NavigationController.getInstance().loadLoginView();
+    }
+
+    // Create all types of subscribe 
+    private void createTypeOfSubscribeInstances() {
+        TypeSubscribeFacade typeSubscribeFacade = TypeSubscribeFacade.getInstance();
+
+        typeSubscribeFacade.createTypeOfSubscribe("Basique", 0, Integer.MAX_VALUE);
+        typeSubscribeFacade.createTypeOfSubscribe("Mensuel", 4.99, 30);
+        typeSubscribeFacade.createTypeOfSubscribe("Annuel", 29.99, 365);
     }
 }
