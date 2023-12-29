9aabf22602d9859e5fedd54df7cecd6b2da84ada
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-12-29 09:52:13 +0100

    ✨ feat: Display all informations from TypeSubscribe facade into the subscribe page

--- src/main/java/com/moodvie/controller/subscribeController/SubscribeController.java
+++ src/main/java/com/moodvie/controller/subscribeController/SubscribeController.java
@@ -1,5 +1,13 @@
 package com.moodvie.controller.subscribeController;
 
+import java.util.ArrayList;
+
+import com.moodvie.business.facade.SubscribeFacade;
+import com.moodvie.business.facade.TypeSubscribeFacade;
+import com.moodvie.persistance.model.Subscribe;
+import com.moodvie.persistance.model.TypeOfSubscribe;
+
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,7 +23,10 @@ public class SubscribeController {
     private VBox boxBaseSubscribe, boxMonthlySubscribe, boxAnnualSubscribe;
 
     @FXML
-    private Label text1, text2, text3;
+    private Label titleTypeOfSubscribeBase, titleTypeOfSubscribeMonthly, titleTypeOfSubscribeAnnual;
+
+    @FXML
+    private Label priceTypeOfSubscribeBase, priceTypeOfSubscribeMonthly, priceTypeOfSubscribeAnnual;
 
     @FXML
     private Button button1, button2;
@@ -29,9 +40,16 @@ public class SubscribeController {
     @FXML
     private VBox boxActionBaseSubscribe, boxActionMonthlySubscribe, boxActionAnnualSubscribe;
 
+    // Inject the subscribe facade and the type of subscribe facade
+    private final SubscribeFacade subscribeFacade = SubscribeFacade.getInstance();
+    private final TypeSubscribeFacade typeSubscribeFacade = TypeSubscribeFacade.getInstance();
+
     @FXML
     public void initialize() {
         hBox.setFillHeight(false);
+
+        // Get informations from facades
+        handleTypeSubscribeFacade();
 
         setupSubscribeBox(boxBaseSubscribe, boxTitleBaseSubscribe, boxFeaturesBaseSubscribe, boxActionBaseSubscribe);
         setupSubscribeBox(boxMonthlySubscribe, boxTitleMonthlySubscribe, boxFeaturesMonthlySubscribe, boxActionMonthlySubscribe);
@@ -41,6 +59,47 @@ public class SubscribeController {
         setupButtonEventHandler(button2, "Button 2 clicked!");
     }
 
+    // Get all the types of subscribe
+    public void handleTypeSubscribeFacade() {
+
+        // Get all the types of subscribe
+        ArrayList<TypeOfSubscribe> typeOfSubscribeList = typeSubscribeFacade.getListTypeOfSubscribe();
+
+        for(TypeOfSubscribe typeOfSubscribe : typeOfSubscribeList) {
+            String label = typeOfSubscribe.getLabel();
+            switch (label) {
+                case "Basique":
+                    titleTypeOfSubscribeBase.setText("Abonnement " + label);
+                    priceTypeOfSubscribeBase.setText("Gratuit");
+                    displayFeatures(typeOfSubscribe.getFeatures(), boxFeaturesBaseSubscribe);
+                    break;
+                case "Mensuel":
+                    titleTypeOfSubscribeMonthly.setText("Abonnement " + label);
+                    priceTypeOfSubscribeMonthly.setText(typeOfSubscribe.getPrice() + "€" + "/mois");
+                    displayFeatures(typeOfSubscribe.getFeatures(), boxFeaturesMonthlySubscribe);
+                    break;
+                case "Annuel":
+                    titleTypeOfSubscribeAnnual.setText("Abonnement " + label);
+                    priceTypeOfSubscribeAnnual.setText(typeOfSubscribe.getPrice() + "€" + "/an");
+                    displayFeatures(typeOfSubscribe.getFeatures(), boxFeaturesAnnualSubscribe);
+                    break;
+            }
+        }
+    }
+
+    // Display features in the specified featuresBox
+    private void displayFeatures(ArrayList<String> features, VBox featuresBox) {
+        // Clear existing content
+        featuresBox.getChildren().clear(); 
+
+        // Add labels to the VBox
+        for (String feature : features) {
+            Label featureLabel = new Label(feature);
+            featuresBox.getChildren().add(featureLabel);
+        }
+    }
+
+    // Set up the layout of the subscribe box
     private void setupSubscribeBox(VBox box, VBox titleBox, VBox featuresBox, VBox actionBox) {
         box.prefWidthProperty().bind(hBox.widthProperty().multiply(0.25));
         box.prefHeightProperty().bind(hBox.heightProperty().multiply(0.9));
@@ -49,10 +108,12 @@ public class SubscribeController {
         actionBox.prefHeightProperty().bind(box.heightProperty().multiply(0.25));
     }
 
+    // Set up the event handler for the button
     private void setupButtonEventHandler(Button button, String message) {
         button.setOnAction(event -> handleButtonClick(message));
     }
 
+    // Handle the button click event
     private void handleButtonClick(String message) {
         System.out.println(message);
     }
